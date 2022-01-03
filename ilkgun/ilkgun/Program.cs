a1cecd9fce54a6410f2d48b9044f8f3086c6c91d
Enes Çetintaş <enesctnts@gmail.com>
2022-01-03 12:11:54 +0300

    ilk gun

---
+++ ilkgun/ilkgun/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ilkgun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dünya!");
+
+        }
+    }
+}
