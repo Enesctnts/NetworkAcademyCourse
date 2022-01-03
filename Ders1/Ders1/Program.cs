d9b2964f2e40f147f69504fbc6f309d3ba2203f1
Enes Çetintaş <enesctnts@gmail.com>
2022-01-03 14:37:58 +0300

    Mükemmel Sayi Bulma

---
+++ Ders1/Ders1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ders1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mükemmel sayiyi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int toplam = 0;
+            for (int i = 1 ; i < sayi; i++)
+            {
+                if (sayi%i==0)
+                {
+                    toplam += i;
+                }
+            }
+            if (toplam == sayi)
+            {
+                Console.WriteLine("Girilen sayi MÜKEMMEL sayidir.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayi MÜKEMMEL sayi değildir");
+            }
+        }
+    }
+}
