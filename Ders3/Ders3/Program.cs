8d8ce8dacc3e8f4f3bb07e1df0564b7ddc0d8687
Enes Çetintaş <enesctnts@gmail.com>
2022-01-06 11:17:23 +0300

    06.01.2022

---
+++ Ders3/Ders3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Ders3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int say = 0;
+            sayalim:
+            if (say<=1000)
+            {
+                Console.WriteLine("Sıradaki sayi: " + say);
+                say++;
+                goto sayalim;
+            }
+            else
+            {
+                Console.WriteLine("Bitti Kardeş Uzatma");
+            }
+            
+
+            int fak = 1;
+
+            Console.Write("Faktoriyel alıncak sayiyi giriniz: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            devamet:
+            if (1<n)
+            {
+                fak = fak * n;
+                n--;
+                goto devamet;
+            }
+            Console.WriteLine("Faktöriyel: " + fak);
+
+            
+            Console.Write("Sayiyi Giriniz: ");
+            int taban = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Üstünü Giriniz: ");
+            int ust = int.Parse(Console.ReadLine());
+            int üst = ust;
+            int sonuc = 1;
+            devamke:
+            sonuc = sonuc * taban;
+            ust--;
+            if (0<ust)
+            {
+                goto devamke;
+
+            }  
+            Console.WriteLine(taban + " ^ " + üst + " = " + sonuc );
+            */
+
+
+
+
+
+
+
+
+        }
+    }
+}
