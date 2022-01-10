cc46a04bfa8c3604b1d96581abbac1ddf7946f05
Enes Çetintaş <enesctnts@gmail.com>
2022-01-10 14:28:24 +0300

    10.01.2022

---
+++ ders7/ders7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace ders7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] puanlar = { 10, 20, 30, 40, 10, 14, 10, 20, 30, 90 };
+            //int startIndex = 0;
+            //int adet10 = puanlar.Count(sayi => sayi == 10);
+
+            //for (int i = 0; i < adet10; i++)
+            //{
+            //    startIndex = Array.IndexOf(puanlar, 10, startIndex); // puanlar dizisinde starIndexten(0) den başlayıp 10 değerini ara ve buldugun indexi al. 
+            //    Console.WriteLine("10 değeri " + startIndex + ". indexte var.");
+            //    startIndex++;// burada index sayisini 1 artırıyoruz çünkü aynı indexsi vermesin sürekli.
+            //}
+            //int[] puanlar = { 10, 20, 30, 40, 10, 14, 10, 20, 30, 90 };
+
+            //for (int i = 0; i < puanlar.Length; i++)
+            //{
+            //   if(puanlar[i]==10)
+            //    {
+            //        Console.WriteLine("10 değeri " + i + ". indexte var.");
+            //    }
+
+            //}
+            //int[] puanlar = { 10, 21, 32, 43, 1, 15, 18, 26, 3, 88,12,19,66 };
+            //int çift = 0;
+            //for (int i = 0; i < puanlar.Length; i++)
+            //{
+            //    if (puanlar[i] % 2 == 0)
+            //    {
+            //        çift++;
+            //        Console.WriteLine("Dizideki Çift Sayilar= "+ puanlar[i]);
+            //    }
+            //}
+            
+            //Console.WriteLine("Çift Adet= " + çift);
+
+
+
+                Console.ReadLine();
+
+
+        }
+    }
+}
