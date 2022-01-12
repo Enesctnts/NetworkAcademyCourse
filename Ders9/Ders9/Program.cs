1911d70398cadc0322371ad6ca32899b84365d0c
Enes Çetintaş <enesctnts@gmail.com>
2022-01-12 14:47:50 +0300

    12.01.22

---
+++ Ders9/Ders9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ders9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayiyi Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sayi+ "!= " + faktoriyel(sayi));
+            
+        }
+        static int faktoriyel(int sayi)
+        {
+            int fak = 1;
+            
+            for (int i = 2; i <= sayi; i++)
+            {
+                fak = fak * i;
+
+            }
+            return fak;
+        }
+    }
+}
